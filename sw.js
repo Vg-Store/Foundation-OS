9b63d9846d3dc1679d18e72120d0a3be1d941730
Vg <vamang891@gmail.com>
2026-09-19 16:08:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,15 @@
-const CACHE = 'foundation-os-v3';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+const CACHE = 'ascent-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(cached=>{
-      return cached || fetch(e.request).then(res=>{
-        if(e.request.method === 'GET' && res.ok){
-          const clone = res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request, clone));
-        }
-        return res;
-      }).catch(()=>cached);
-    })
-  );
+/* stale-while-revalidate: instant/offline loads, updates picked up on next open */
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || new URL(e.request.url).origin !== location.origin) return;
+  e.respondWith(caches.open(CACHE).then(async c => {
+    const cached = await c.match(e.request, { ignoreSearch: true });
+    const net = fetch(e.request).then(r => { if (r.ok) c.put(e.request, r.clone()); return r; }).catch(() => cached);
+    return cached || net;
+  }));
 });
